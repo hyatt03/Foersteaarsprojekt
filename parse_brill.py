7597cb91ce512c4f81a07231056fc90dfc7edd54
Jonas Peter Hyatt <jph@dbc.dk>
2017-03-07 09:27:39 +0100

    initial optimizer

--- parse_brill.py
+++ parse_brill.py
@@ -3,6 +3,11 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import subprocess
+import time
+
+FNULL = open(os.devnull, 'w')
 
 plt.rcParams["figure.figsize"] = (15,8)
 
@@ -35,36 +40,108 @@ def compare(row_a, row_comp):
     
     return res
 
-# Read files
-mean_begin = read_file('./brill_ref', 'Mean_brill_begin.dat')
-peak_begin = read_file('./brill_ref', 'Peak_brill_begin.dat')
+def process_brilliance(path, type):
+    # Read files
+    # data_begin = read_file('./brill_ref', '{}_brill_begin.dat'.format(type))
 
-mean_end = read_file(sys.argv[1], 'Mean_brill_end.dat')
-peak_end = read_file(sys.argv[1], 'Peak_brill_end.dat')
+    data_begin = read_file(path, '{}_brill_begin.dat'.format(type))
+    data_end = read_file(path, '{}_brill_end.dat'.format(type))
 
-# Process the files
-peak_comp = np.divide(peak_end, peak_begin)
-mean_comp = np.divide(mean_end, mean_begin)
+    # Process the files
+    # Ignore divide by zero, we'll fix it later
+    with np.errstate(divide='ignore', invalid='ignore'):
+        comp = np.divide(data_end, data_begin)
+        
+    result = np.zeros((row_count, col_count))
 
-peak_result = np.zeros((row_count, col_count))
-mean_result = np.zeros((row_count, col_count))
+    # Normalize the data and add correct x axis
+    for i in xrange(0, row_count):
+        result[i] = compare(data_begin[i], comp[i])
 
-for i in xrange(0, row_count):
-    peak_result[i] = compare(peak_begin[i], peak_comp[i])
-    mean_result[i] = compare(mean_begin[i], mean_comp[i])
+    # Split and transpose for ease of further calculations.
+    res_x = result[:, 0:1].T[0]
+    res_y = result[:, 1:2].T[0]
 
-area_peak = np.trapz(y = peak_result[:, 1:2].T[0], x = peak_result[:, 0:1].T[0])
-area_mean = np.trapz(y = mean_result[:, 1:2].T[0], x = mean_result[:, 0:1].T[0])
+    # Calculate the area under the curves
+    area = np.trapz(y = res_y, x = res_x)
+    
+    # Return all relevant stuff!
+    return [area, result, res_x, res_y]
+
+def compile_mcstas(instrument):
+    print 'About to optimize {}'.format(instrument)
+    
+    compile_to_c = ['mcstas', '-I', '.', '-t', '-o', './{}.c'.format(instrument), './{}.instr'.format(instrument)]
+    print 'Running: {}'.format(compile_to_c)
+    exc = subprocess.call(compile_to_c, stdout=FNULL, stderr=subprocess.STDOUT)
+    if exc == 0:
+        print 'Compiled to C!'
+    else:
+        print 'Compile to C failed!'
+        sys.exit(1)
+    
+    compile_to_bin = ['cc', '-o', './{}.out'.format(instrument), './{}.c'.format(instrument), '-lm']
+    print 'running: {}'.format(compile_to_bin)
+    exc = subprocess.call(compile_to_bin, stdout=FNULL, stderr=subprocess.STDOUT)
+    if exc == 0:
+        print 'Compiled to binary!'
+    else:
+        print 'Compile to binary failed!'
+        sys.exit(1)
+        
+def run_mcstas(instrument, params):
+    epoch_time = int(time.time() * 1000)
+    save_dir = './data/{}_{}'.format(instrument, epoch_time)
+    print 'Beginning run @ {}'.format(epoch_time)
+    
+    run_instrument_with_params = [
+        './{}.out'.format(instrument), 
+        '-n', '10000000', 
+        '-d', save_dir,
+        'm_val=6',
+        'guide_mid_width={}'.format(params['guide_mid_width']),
+        'guide_mid_height={}'.format(params['guide_mid_height']),
+        'guide_linxw={}'.format(params['guide_linxw']),
+        'guide_loutxw={}'.format(params['guide_loutxw']),
+        'guide_linyh={}'.format(params['guide_linyh']),
+        'guide_loutyh={}'.format(params['guide_loutyh'])
+    ]
+    
+    print 'running: {}'.format(instrument)
+    exc = subprocess.call(run_instrument_with_params, stdout=FNULL, stderr=subprocess.STDOUT)
+    if exc == 0:
+        print 'Successful run!'
+    else:
+        print 'Something went wrong!'
+        sys.exit(1)
+    
+    print 'finished running in {} ms'.format(int(time.time() * 1000) - epoch_time)
+        
+    return process_brilliance(save_dir, 'Mean')
+    
+# Get initial parameters
+parameter_dict = {
+    'guide_mid_width': 0.18,
+    'guide_mid_height': 0.18,
+    'guide_linxw': 10.5,
+    'guide_loutxw': 10.5,
+    'guide_linyh': 10.5,
+    'guide_loutyh': 10.5
+}
+
+compile_mcstas('ess_sim_simple')
+area, result, res_x, res_y = run_mcstas('ess_sim_simple', parameter_dict)
+print area
 
 # Plot results
 fig, ax = plt.subplots()
-ax.plot(peak_result[:, 0:1].T[0], peak_result[:, 1:2].T[0], 'r-', label='Peak brilliance transfer')
-ax.plot(mean_result[:, 0:1].T[0], mean_result[:, 1:2].T[0], 'b-', label='Mean brilliance transfer')
-legend = ax.legend(loc='upper right', shadow=True)
+# ax.plot(peak_res_x, peak_res_y, 'r-', label='Peak brilliance transfer')
+ax.plot(res_x, res_y, 'b-', label='Mean brilliance transfer')
+# legend = ax.legend(loc='upper left', shadow=True)
 
 plt.xlabel("Wavelength [AA]")
 plt.ylabel("Brilliance Transfer")
-plt.title('Peak/Mean brilliance transfers as a function of wavelength\nPeak area: {}, Mean area: {}'.format(area_peak, area_mean))
+# plt.title('Peak/Mean brilliance transfers as a function of wavelength\nPeak area: {}, Mean area: {}'.format(area_peak, area_mean))
 plt.axis([0, 8, 0, 1])
 plt.grid(True)
 plt.show()
