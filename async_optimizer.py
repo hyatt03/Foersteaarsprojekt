840226db228148b8c67604b29d4a8737a7b0d7f2
Jonas Peter Hyatt <jph@dbc.dk>
2017-03-12 00:24:06 +0100

    restructure and improvements

--- async_optimizer.py
+++ async_optimizer.py
@@ -1,24 +1,60 @@
-import sys
-import os
+#!/usr/bin/env python
 
-sys.path.append(os.path.abspath('/home/jph/Foersteaarsprojekt'))
+"""
+@file: This file includes all interactions with hyperopt.
+"""
 
 from hyperopt import hp, fmin, tpe
 from hyperopt.mongoexp import MongoTrials
+import multiprocessing as mp
+from subprocess import Popen
+import json
+
+from mcstas_utils import getExperiment, now, getLimits, compile_mcstas
+from simulation_analysis_utils import objective, getWorkingDir, getResultsFile
+
+# Declare some helpful variables.
+workerDb = 'localhost:1234/exp_db'
+masterDb = 'mongo://{}/jobs'.format(workerDb)
+
+# Converts a simple dict to a hyperopt search space
+def getSearchSpace(parameterLimits):
+    choice_space = []
+
+    for key in parameterLimits.keys():
+        choice_space.append(hp.uniform(key, parameterLimits[key][0], parameterLimits[key][1]))
     
-from parse_brill import getSearchSpace, getExperiment, now, objective
+    return choice_space
 
-exp_label = '{}_{}'.format(getExperiment(), now())
-limitsDict = {
-    'guide_mid_width': [0, 0.1],
-    'guide_mid_height': [0, 0.1],
-    'guide_linxw': [0, 80],
-    'guide_loutxw': [0, 80],
-    'guide_linyh': [0, 80],
-    'guide_loutyh': [0, 80]
-}
+# Run the optimization using hyperopt.
+def runOptimizations(evals):
+    limitsDict = getLimits()
+    exp_label = '{}_{}'.format(getExperiment(), now())
+    
+    # Allow parallelizing 
+    trials = MongoTrials(workerDb, exp_key=exp_label)
+    
+    # Use hyperopt to optimize function
+    best = fmin(objective, getSearchSpace(limitsDict), trials=trials, algo=tpe.suggest, max_evals=evals)
+    
+    # Write results to a file
+    with open(getResultsFile(), "a") as myfile:
+        myfile.write('{}\n'.format(json.dumps(best)))
 
-trials = MongoTrials('mongo://localhost:1234/exp_db/jobs', exp_key=exp_label)
-best = fmin(objective, getSearchSpace(limitsDict), trials=trials, algo=tpe.suggest, max_evals=100)
-with open("best.txt", "a") as myfile:
-    myfile.write(str(best))
+    return best
+
+# Open a worker for each idle CPU.
+def startWorkers():
+    workerBoot = ['PYTHONPATH=$PYTHONPATH:{}/'.format(getWorkingDir()), 'hyperopt-mongo-worker', '--mongo={}'.format(workerDb), '--poll-interval=1']
+    for i in range(mp.cpu_count()):
+        Popen(workerBoot, shell = True, stdin = None, stdout = None, stderr = None, close_fds = True)
+
+if __name__ == "__main__":
+    # Compile the experiment first, this ensures it's always up to date
+    compile_mcstas(getExperiment())
+    
+    # Start a worker for each core on the system
+    startWorkers()
+    
+    # Run the actual optimization.
+    best = runOptimizations(100)
---
+++ mcstas_utils.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+"""
+@file: In this file some helper methods for running McStas within python.
+"""
+
+import os
+import subprocess
+import time
+
+FNULL = open(os.devnull, 'w')
+limitsDict = {
+    'guide_mid_width': [0, 0.1],
+    'guide_mid_height': [0, 0.1],
+    'guide_linxw': [0, 80],
+    'guide_loutxw': [0, 80],
+    'guide_linyh': [0, 80],
+    'guide_loutyh': [0, 80]
+}
+
+def getExperiment():
+    return 'ess_sim_simple'
+
+def getLimits():
+    return limitsDict
+
+def now():
+    return int(time.time() * 100000)
+
+# Compiles an instrument to a binary file.
+def compile_mcstas(instrument):
+    workingDir = getWorkingDir()
+    print 'About to compile {}'.format(instrument)
+    
+    compile_to_c = ['mcstas', '-I', '.', '-t', '-o', '{}/{}.c'.format(workingDir, instrument), '{}/{}.instr'.format(workingDir, instrument)]
+    print 'Running: {}'.format(compile_to_c)
+    exc = subprocess.call(compile_to_c, stdout=FNULL, stderr=subprocess.STDOUT)
+    if exc == 0:
+        print 'Compiled to C!'
+    else:
+        print 'Compile to C failed!'
+        sys.exit(1)
+
+    compile_to_bin = ['cc', '-o', '{}/{}.out'.format(workingDir, instrument), '{}/{}.c'.format(workingDir, instrument), '-lm']
+    print 'running: {}'.format(compile_to_bin)
+    exc = subprocess.call(compile_to_bin, stdout=FNULL, stderr=subprocess.STDOUT)
+    if exc == 0:
+        print 'Compiled to binary!'
+    else:
+        print 'Compile to binary failed!'
+        sys.exit(1)
+        
+    return exc
+
+# runs a simulation given a set of parameters
+def run_mcstas(instrument, params, neutrons = 10000000, m_val = 2):
+    workingDir = getWorkingDir()
+    epoch_time = now()
+    save_dir = '{}/data/{}_{}'.format(workingDir, instrument, epoch_time)
+    run_instrument_with_params = [
+        '{}/{}.out'.format(workingDir, instrument), 
+        '-n', str(neutrons), 
+        '-d', save_dir,
+        'm_val={}'.format(m_val)
+    ]
+    
+    for key in params.keys():
+        run_instrument_with_params.append('{}={}'.format(key, params[key]))
+
+    exc = subprocess.call(run_instrument_with_params, stdout=FNULL, stderr=subprocess.STDOUT)
+    if exc == 0:
+        print 'Successful run!'
+    else:
+        print 'Something went wrong!'
+        sys.exit(1)
+    
+    print 'finished running in {}'.format(now() - epoch_time)
+        
+    return save_dir
+
+from simulation_analysis_utils import getWorkingDir
