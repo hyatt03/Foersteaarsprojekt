6654173b7bd9c5185cb2442bad10b4a3b497a609
Jonas Peter Hyatt <jph@dbc.dk>
2017-03-14 22:24:14 +0100

    new plots

--- simulation_analysis_utils.py
+++ simulation_analysis_utils.py
@@ -4,8 +4,8 @@
 @file: Utilities for analyzing a simulation that has already been run. Defines objective for practical purposes.
 """
 
-workingDir = '/home/jph/Foersteaarsprojekt'
-# workingDir = '/Users/jonashyatt/Code/Foersteaarsprojekt'
+# workingDir = '/home/jph/Foersteaarsprojekt'
+workingDir = '/Users/jonashyatt/Code/Foersteaarsprojekt'
 def getWorkingDir():
     return workingDir
 
@@ -20,9 +20,10 @@ matplotlib.rcParams.update({'font.size': 24})
 
 # Import helper modules
 import matplotlib.pyplot as plt
+# from matplotlib import mpl
 import math
 import json
-from mcstas_utils import getExperiment, now, getLimits, run_mcstas
+from mcstas_utils import getExperiment, now, getLimits, run_mcstas, compile_mcstas
 import numpy as np
 
 # Define size of brilliance data.
@@ -45,6 +46,24 @@ def read_file(path, filename, row_count = 101, col_count = 4):
                 a[i][3] = float(lineArray[3])
                 i = i + 1
                 
+    return a
+
+# Split into pixels and throw into psd array
+def read_psd(save_dir, x = 200, y = 200):
+    a = []
+    with open(save_dir) as f:
+        i = 0
+        for line in f:
+            if not '#' in line:
+                a.append([])
+                lineArray = line.split(' ')
+                for pixel in lineArray:
+                    try:
+                        float(pixel)
+                        a[i].append(float(pixel))
+                    except ValueError:
+                        'nothing'
+                i = i + 1
     return a
 
 def fixDivisionByZeroAndIndex(row_a, row_comp):
@@ -99,26 +118,72 @@ def process_brilliance(path, type, row_count = 101, col_count = 4):
     # Return all relevant stuff!
     return [area, result, res_x, res_y, err_y]
 
+def plotPSD(save_dir, xmax = 2, ymax = 2, filename = 0):
+    fig, ax = plt.subplots()
+    z = read_psd(save_dir)
+    dim = len(z[0])
+    z = z[0:dim]
+    
+    x = np.linspace(-xmax, xmax, num = dim)
+    y = np.linspace(-ymax, ymax, num = dim)
+
+    plt.pcolor(x, y, z)
+    plt.colorbar()
+    
+    plt.xlabel("X-Position [cm]")
+    plt.ylabel("Y-Position [cm]")
+    
+    plt.title('Intesity as a function of position')
+    
+    if filename == 0:
+        filename = 'psd_{}.png'.format(now())
+    plt.savefig(filename)
+    
+    return filename
+
+def plotDIV(save_dir, xmax = 2, ymax = 2, filename = 0):
+    fig, ax = plt.subplots()
+    z = read_psd(save_dir)
+    dim = len(z[0])
+    z = z[0:dim]
+    
+    x = np.linspace(-xmax, xmax, num = dim)
+    y = np.linspace(-ymax, ymax, num = dim)
+    
+    plt.pcolor(x, y, z)
+    plt.colorbar()
+    
+    plt.xlabel("Horizontal Divergence [degrees]")
+    plt.ylabel("Vertical Divergence [degrees]")
+    
+    plt.title('Intensity as a function of 2d divergence')
+    
+    if filename == 0:
+        filename = 'div_{}.png'.format(now())
+    plt.savefig(filename)
+    
+    return filename
+
 def plotBT(instrument, params):
     # Run sim to get all the required data.
-    save_dir = run_mcstas(instrument, params)
+    save_dir = run_mcstas(instrument, params, neutrons = 100000000)
     [mean_area, mean_result, res_x, res_y, err_y] = process_brilliance(save_dir, 'Mean')
-    [peak_area, peak_result, peak_res_x, peak_res_y, peak_err_y] = process_brilliance(save_dir, 'Peak')
+    # [peak_area, peak_result, peak_res_x, peak_res_y, peak_err_y] = process_brilliance(save_dir, 'Peak')
 
     # Plot the data
     fig, ax = plt.subplots()
 
-    ax.plot(peak_res_x, peak_res_y, 'r.-', label='Peak brilliance transfer')
+    # ax.plot(peak_res_x, peak_res_y, 'r.-', label='Peak brilliance transfer')
     ax.plot(res_x, res_y, 'b.-', label='Mean brilliance transfer')
 
     legend = ax.legend(loc='upper left', shadow=True)
 
     plt.xlabel("Wavelength [AA]")
     plt.ylabel("Brilliance Transfer")
-    plt.title('Peak/Mean brilliance transfers as a function of wavelength\nPeak area: {:3.2f}, Mean area: {:3.2f}'.format(peak_area, mean_area))
+    plt.title('Mean brilliance transfers as a function of wavelength')
     plt.axis([0, 8, 0, 1])
     plt.grid(True)
-    plt.savefig('optimized_mean_{}.png'.format(now()))
+    plt.savefig('ess_brill_optimized_mean_{}.png'.format(now()))
 
     return save_dir
 
@@ -151,4 +216,11 @@ if __name__ == "__main__":
     with open(getResultsFile(), "rb") as a:
         lines = a.readlines()
 
+    compile_mcstas(getExperiment())
     save_dir = plotBT(getExperiment(), json.loads(lines[-1]))
+    plotDIV(save_dir + '/source_div.dat', filename = 'div_before_ess_brill_optimized.png')
+    plotDIV(save_dir + '/sample_div.dat', filename = 'div_after_ess_brill_optimized.png')
+    
+    plotPSD(save_dir + '/source_psd.dat', filename = 'psd_before_ess_brill_optimized.png')
+    plotPSD(save_dir + '/sample_psd.dat', filename = 'psd_after_ess_brill_optimized.png')
+    
